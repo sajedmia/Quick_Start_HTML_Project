4e15e026302f0c5fb482154908252daa61ae0597
sajedmia <48439335+sajedmia@users.noreply.github.com>
2020-04-22 08:12:05 +0600

    Add files via upload

---
+++ pName/assets/js/theme.js
@@ -0,0 +1,19 @@
+//For Custom JS
+(function($) {
+'use strict';
+
+//On Ready Function is here
+$(document).ready(function(){
+
+	
+
+});
+
+//On Load Function is here
+$(window).load(function() {
+
+	
+
+});
+
+})(jQuery);
